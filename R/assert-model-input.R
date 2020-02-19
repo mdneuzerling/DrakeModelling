061cba5ab82a95f427d5a7ae79f2a4e90ddb3064
Murray David Neuzerling <murray@neuzerling.com>
2020-02-20 09:25:32 +1100

    Trimming project to vocabulary

--- R/assert-model-input.R
+++
@@ -1,9 +0,0 @@
-# context("sentiment")
-# 
-# test_that("'bad' is bad sentiment", {
-#   expect_equal(sentiment("bad"), "bad")
-# })
-# 
-# test_that("'love' is good sentiment", {
-#   expect_equal(sentiment("love"), "good")
-# })
