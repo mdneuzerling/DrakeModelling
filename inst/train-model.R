327be6ee1022b25d0dc04726a1c2e9faef27e186
Murray David Neuzerling <murray@neuzerling.com>
2020-02-20 10:15:36 +1100

    Exposing imports in plans

--- inst/train-model.R
+++ inst/train-model.R
@@ -1,4 +1,4 @@
 devtools::load_all()
-# package_name <- basename(here::here())
-# drake::expose_imports(package_name, character_only = TRUE)
+package_name <- basename(here::here())
+drake::expose_imports(package_name, character_only = TRUE)
 drake::make(model_training_plan())
